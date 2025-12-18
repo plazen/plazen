6fcb2dbd5f06bd7acca66cc0dbf80a2e5b8dfa7b
drew <me@andrinoff.com>
2025-12-18 23:57:15 +0400

    ci: change release file

--- .releaserc.js
+++ .releaserc.js
@@ -20,98 +20,101 @@ module.exports = {
     [
       "@semantic-release/release-notes-generator",
       {
-        writerOpts: {
-          issueUrlFormat: "{{host}}/{{owner}}/{{repository}}/issues/{{id}}",
-          commitUrlFormat: "{{host}}/{{owner}}/{{repository}}/commit/{{hash}}",
-          compareUrlFormat:
-            "{{host}}/{{owner}}/{{repository}}/compare/{{previousTag}}...{{currentTag}}",
-          userUrlFormat: "{{host}}/{{user}}",
+        writerOpts: (function () {
+          const contributors = new Map();
+          return {
+            issueUrlFormat: "{{host}}/{{owner}}/{{repository}}/issues/{{id}}",
+            commitUrlFormat:
+              "{{host}}/{{owner}}/{{repository}}/commit/{{hash}}",
+            compareUrlFormat:
+              "{{host}}/{{owner}}/{{repository}}/compare/{{previousTag}}...{{currentTag}}",
+            userUrlFormat: "{{host}}/{{user}}",
 
-          groupBy: "type",
-          commitGroupsSort: "title",
-          commitsSort: ["scope", "subject"],
-          transform: (commit, context) => {
-            context._contributors = context._contributors || new Map();
+            groupBy: "type",
+            commitGroupsSort: "title",
+            commitsSort: ["scope", "subject"],
+            transform: (commit, context) => {
+              const type = commit.type || "other";
+              const newCommit = Object.assign({}, commit);
 
-            const type = commit.type || "other";
+              newCommit.emoji = emojiForType[type] || "🔹";
 
-            commit.emoji = emojiForType[type] || "🔹";
+              if (!newCommit.originalType) {
+                newCommit.originalType = newCommit.type;
+              }
+              newCommit.type = `${newCommit.emoji} ${newCommit.originalType || type}`;
 
-            if (!commit.originalType) {
-              commit.originalType = commit.type;
-            }
-            commit.type = `${commit.emoji} ${commit.originalType || type}`;
+              if (newCommit.subject) {
+                const host = context.host || "https://github.com";
+                const owner =
+                  context.owner ||
+                  (context.repository ? context.repository.split("/")[0] : "");
+                const repo = context.repository || context.packageName || "";
+                newCommit.subject = newCommit.subject.replace(
+                  /(?:\s|^)(#)(\d+)\b/g,
+                  (_, p1, id) => {
+                    return ` [#${id}](${host}/${owner}/${repo}/issues/${id})`;
+                  },
+                );
+                newCommit.subject = newCommit.subject.replace(
+                  /([a-zA-Z0-9_.-]+\/[a-zA-Z0-9_.-]+)#(\d+)/g,
+                  (_, repoPath, id) => {
+                    return `[${repoPath}#${id}](${host}/${repoPath}/issues/${id})`;
+                  },
+                );
+                newCommit.subject = newCommit.subject.replace(
+                  /\(PR\s+#(\d+)\)/gi,
+                  (_, id) => {
+                    return `(PR [#${id}](${host}/${owner}/${repo}/pull/${id}))`;
+                  },
+                );
+              }
 
-            if (commit.subject) {
-              const host = context.host || "https://github.com";
-              const owner =
-                context.owner ||
-                (context.repository ? context.repository.split("/")[0] : "");
-              const repo = context.repository || context.packageName || "";
-              commit.subject = commit.subject.replace(
-                /(?:\s|^)(#)(\d+)\b/g,
-                (_, p1, id) => {
-                  return ` [#${id}](${host}/${owner}/${repo}/issues/${id})`;
-                },
-              );
-              commit.subject = commit.subject.replace(
-                /([a-zA-Z0-9_.-]+\/[a-zA-Z0-9_.-]+)#(\d+)/g,
-                (_, repoPath, id) => {
-                  return `[${repoPath}#${id}](${host}/${repoPath}/issues/${id})`;
-                },
-              );
-              commit.subject = commit.subject.replace(
-                /\(PR\s+#(\d+)\)/gi,
-                (_, id) => {
-                  return `(PR [#${id}](${host}/${owner}/${repo}/pull/${id}))`;
-                },
-              );
-            }
-
-            const author = commit.author || commit.committer || {};
-            if (author) {
-              const username = author.username || author.name || author.login;
-              if (username) {
-                let url;
-                if (author.username || author.login) {
-                  url = (context.host || "https://github.com") + "/" + username;
-                } else if (author.email) {
-                  url = `mailto:${author.email}`;
-                }
-                if (!context._contributors.has(username)) {
-                  context._contributors.set(username, {
-                    name: author.name || username,
-                    username,
-                    url,
-                  });
+              const author = newCommit.author || newCommit.committer || {};
+              if (author) {
+                const username = author.username || author.name || author.login;
+                if (username) {
+                  let url;
+                  if (author.username || author.login) {
+                    url =
+                      (context.host || "https://github.com") + "/" + username;
+                  } else if (author.email) {
+                    url = `mailto:${author.email}`;
+                  }
+                  if (!contributors.has(username)) {
+                    contributors.set(username, {
+                      name: author.name || username,
+                      username,
+                      url,
+                    });
+                  }
                 }
               }
-            }
-            return commit;
-          },
-          finalizeContext: (context) => {
-            const contributors = context._contributors || new Map();
-            if (contributors.size > 0) {
+              return newCommit;
+            },
+            finalizeContext: (context) => {
               const lines = [];
-              for (const [, info] of contributors) {
-                if (info.url && info.url.startsWith("http")) {
-                  lines.push(
-                    `- [${info.username}](${info.url})${info.name && info.name !== info.username ? ` (${info.name})` : ""}`,
-                  );
-                } else {
-                  lines.push(`- ${info.name || info.username}`);
+              if (contributors.size > 0) {
+                for (const [, info] of contributors) {
+                  if (info.url && info.url.startsWith("http")) {
+                    lines.push(
+                      `- [${info.username}](${info.url})${info.name && info.name !== info.username ? ` (${info.name})` : ""}`,
+                    );
+                  } else {
+                    lines.push(`- ${info.name || info.username}`);
+                  }
                 }
               }
-              context.customContributors = lines.join("\n");
-            } else {
-              context.customContributors = "";
-            }
-            context.host = context.host || "https://github.com";
-            return context;
-          },
-          footerPartial:
-            "{{#if customContributors}}\n\n### 👥 Contributors\n\n{{{customContributors}}}\n{{/if}}",
-        },
+              const customContributors = lines.length ? lines.join("\n") : "";
+              return Object.assign({}, context, {
+                customContributors,
+                host: context.host || "https://github.com",
+              });
+            },
+            footerPartial:
+              "{{#if customContributors}}\n\n### 👥 Contributors\n\n{{{customContributors}}}\n{{/if}}",
+          };
+        })(),
       },
     ],
 
